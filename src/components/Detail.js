1572e136d5d3af195a1b0fe6234da91acafcd412
Hayeon Kang <hy_k@kakao.com>
2022-05-26 13:49:39 +0900

    20210323 강하연 과제 제출

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -14,7 +14,22 @@ function Detail() {
   });
   
   if (location.state) {
-    return <span>{location.state.title}</span>;
+    return (
+      <div className='de_movie'>
+          <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+          <div className='de_movie__data'>
+            <h3 className='de_movie__title'>
+              {location.state.title}
+            </h3>
+            <ul className='de_movie__genres'>
+              {location.state.genres.map((genre, index) => {
+                return <li key={index} className='de_movie__genre'>{genre}</li>
+              })}
+            </ul>
+            <p className='de_movie_summary'>{location.state.summary}</p>
+          </div>
+        </div>
+    );
   } else {
     return null;
   }
